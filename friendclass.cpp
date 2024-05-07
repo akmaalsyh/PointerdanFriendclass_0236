ef0e010907e14b69fd5db1bb6416322eb7aaa8a5
akmaalsyh <taufansyah50@gmail.com>
2024-05-07 15:53:22 +0700

    memasukkan void siswa orang dan int main

--- friendclass.cpp
+++ friendclass.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class siswa;
@@ -23,3 +24,28 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+
+int main ()
+{
+    orang joko;
+    joko.setNama("Joko Susilo");
+    siswa joko_siswa;
+    joko_siswa.setId(1);
+    joko_siswa.displayAll(joko);
+    return 0;
+}
