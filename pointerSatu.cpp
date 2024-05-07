a8a948ae5b557299be2395fc62ea28feca9fc839
akmaalsyh <taufansyah50@gmail.com>
2024-05-07 15:27:46 +0700

    memasukkan int main mahasiswa nim dan show nim

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -12,3 +12,18 @@ public :
         cout << "No Induk = " << nim << endl;
     }
 };
+
+int main ()
+{
+    mahasiswa mhs{1};   // Object mhs
+    mhs.showNim();      // Member Access Operator
+
+    mahasiswa &refMhs = mhs;  // Pointer Reference refMhs
+    refMhs.nim = 2;     // Member Access Operator
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs; // Pointer deference pMhs
+    pMhs->nim = 3;      // Arrow operator
+    pMhs->showNim();
+    return 0;
+}
